555a2572103849a0417f4970a3785369d604b05d
jkramer <jkramer@ukaachen.de>
2024-07-25 15:17:03 +0200

    Mustache add Views directory added

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const http = require("http");
+const consolidate = require('consolidate')
 
 var app = express();
 const port = 3000
@@ -16,10 +16,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.engine('html',consolidate.mustache)
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, 'views'));
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-module.exports = app;
+
 
 
 var server = http.createServer(app);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
+const {join} = require("node:path");
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { message: 'Hello World' });
 });
+
+router.get('/info', function(req, res, next) {
+  res.render('info');
+})
 
 module.exports = router;
