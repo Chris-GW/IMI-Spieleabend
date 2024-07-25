3d21825bfa26d64ea4b5e96c264c4601b75a7075
Chris-GW <Chris-GW@users.noreply.github.com>
2024-07-25 12:12:20 +0200

    integrate bootstrap

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const port = 3000
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
